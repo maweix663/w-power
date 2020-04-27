1810dfd1b11363cf13469ba97a18f7452cca21eb
马威 <mawei@maweideMacBook-Pro.local>
2020-04-27 17:10:48 +0800

    组件开发 马威

--- src/view/mProduction/stepOne.vue
+++ src/view/mProduction/stepOne.vue
@@ -1,59 +1,35 @@
 <template>
-  <div class="step" >
-    <div class="box">
-      <div class="head">
-        关键指标
-        <p><i class="el-icon-warning-outline"></i>指标说明</p>
-      </div>
-
-      <div class="body"></div>
-    </div>
-
+  <div class="stepContent" >
+    <indicators /> 
   </div>
 </template>
 
 <script>
-  export default {
-    name: "step",
-    data() {
-      return {
-        
-      }
-    },
-    created() {
-
-    },
-    mounted: function () {
-
-    },
-    methods: {
+import indicators from './templates/indicators'
+export default {
+  name: "stepContent",
+  components: {
+    indicators
+  },
+  data() {
+    return {
       
-    },
-  }
+    }
+  },
+  created() {
+
+  },
+  mounted: function () {
+
+  },
+  methods: {
+    
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-.step {
-  .box {
-    overflow: hidden;
-    border-radius: 5px;
-
-    .head {
-      position: relative;
-      line-height: 40px;
-      font-size: 16px;
-      background: #4f8985;
-      color: #fff;
-      padding-left: 10px;
-
-      p {
-        cursor: pointer;
-        top: 0;
-        font-size: 12px;
-        position: absolute;
-        right: 10px;
-      }
-    }
-  }
+.stepContent {
+  
 }
 </style>
